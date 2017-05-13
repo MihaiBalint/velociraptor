c995201a15ad8ac72767970e836394fe57c2bf79
Mihai Balint <balint.mihai@gmail.com>
2017-05-14 00:49:31 +0300

    Remove the last clever bits, start rebuilding.

--- app.js
+++ app.js
@@ -1,42 +1,10 @@
-var utils        = require('utils')
-  , chalk        = require('chalk')
-  , env          = utils.bootstrapEnv()
-  , app          = env.app
-  , debug        = env.debug
-  , helpers      = utils.helpers
-  , bodyParser   = require('body-parser')
-  , moduleLoader = env.moduleLoader;
+var express = require('express')
+var app = express()
 
-moduleLoader.on('beforeLoad', function setupExpress() {
-  debug('Configuring express application...');
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-  app.use(require('morgan')('dev'));
-  app.use(require('response-time')());
-  app.use(require('method-override')());
-  app.use(require('cors')(env.config.cors));
-  app.use(require('compression')());
-
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-});
-
-moduleLoader.on('modulesLoaded', function initializeRoutes() {
-  debug('Initializing routes...');
-  moduleLoader.initializeRoutes();
-});
-
-moduleLoader.on('routesInitialized', function startApp() {
-  debug('Starting http server...');
-
-  app.listen(env.webPort, function appReady() {
-    debug('Listening on port %s', chalk.yellow(env.webPort));
-  });
-});
-
-debug('Loading %s hook files...', chalk.yellow('app.js'));
-helpers.loadModulesFileByName(__filename, env.config, require('cluster'), debug);
-
-debug('Loading modules...');
-moduleLoader.loadModules();
-
-module.exports = app;
+app.listen(8080, function () {
+  console.log('Velociraptor listening on port 8080!')
+})
